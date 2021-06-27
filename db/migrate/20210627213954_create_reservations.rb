afc7cefd94e0a53e4158710c82b0b45fd1ff1286
Kazuya-Sakashita <kz0508@gmail.com>
2021-06-28 07:20:30 +0900

    add simple_calendar and bootstrap

---
+++ db/migrate/20210627213954_create_reservations.rb
@@ -0,0 +1,13 @@
+class CreateReservations < ActiveRecord::Migration[6.1]
+  def change
+    create_table "reservations", force: :cascade do |t|
+      t.date "day", null: false
+      t.string "time", null: false
+      t.bigint "user_id", null: false
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+      t.datetime "start_time", null: false
+      t.index ["user_id"], name: "index_reservations_on_user_id"
+    end
+  end
+end
