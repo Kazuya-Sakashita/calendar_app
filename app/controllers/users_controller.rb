e5aafb267493f9aa7a76598a11129f7fe660bc1f
Kazuya-Sakashita <kz0508@gmail.com>
2021-06-29 07:52:10 +0900

    デバッグソフト追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+  def show
+    # @user = User.find(params[:id])
+    @user = User.find(current_user.id)
+    @user_reservations = current_user.reservations.where("day >= ?", Date.current).order(day: :desc)
+    @visit_historys = current_user.reservations.where("day < ?", Date.current).where("day > ?", Date.today << 12).order(day: :desc)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'reservations#index'
   resources :reservations
+  resources :users, :only => [:show]
 end
