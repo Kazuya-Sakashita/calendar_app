e26ab741a29a7367190fabbbc6591355e4670f21
Kazuya-Sakashita <kz0508@gmail.com>
2021-06-30 07:47:52 +0900

    マイページ追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
   def show
     # @user = User.find(params[:id])
     @user = User.find(current_user.id)
-    @user_reservations = current_user.reservations.where("day >= ?", Date.current).order(day: :desc)
-    @visit_historys = current_user.reservations.where("day < ?", Date.current).where("day > ?", Date.today << 12).order(day: :desc)
+    @user_reservations = Reservation.where(user_id:current_user).where("day >= ?", Date.current).order(day: :desc)
+    @visit_historys = Reservation.where(user_id:current_user).where("day < ?", Date.current).where("day > ?", Date.today << 12).order(day: :desc)
+
+    #実力不足で筆者の下の記載を理解できずエラーを解除できなかったので、自分が理解できる上のコードとした(関連性が設定できていない可能性がある)
+    # @user_reservations = current_user.reservations.where("day >= ?", Date.current).order(day: :desc)
+    # @visit_historys = current_user.reservations.where("day < ?", Date.current).where("day > ?", Date.today << 12).order(day: :desc)
   end
 end
